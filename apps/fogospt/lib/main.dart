09247d74826def88c906f670cfb81036a356a3ba
Filipe Barroso <filipe.barroso@gmail.com>
2024-07-21 22:26:54 +0100

    feat: FCM configuration

--- apps/fogospt/lib/main.dart
+++ apps/fogospt/lib/main.dart
@@ -1,8 +1,88 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:fogos_api/shared/dependency_injection.dart';
 import 'package:fogospt/app.dart';
+import 'package:fogospt/firebase_options.dart';
+import 'package:rxdart/rxdart.dart';
 
-void main() {
+// used to pass messages from event handler to the UI
+final _messageStreamController = BehaviorSubject<RemoteMessage>();
+
+final messaging = FirebaseMessaging.instance;
+
+const topic = 'warning_fogos';
+
+Future<void> main() async {
   setupFogosAPIDependencyInjection();
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await requestingPermissionsFirebaseMessaging();
+  await registrationFirebaseMessaging();
+  await subscribeToFirebaseMessageTopics();
+
+  printReceivedFirebaseMessages();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
   runApp(const FogosApp());
+}
+
+Future<void> subscribeToFirebaseMessageTopics() async {
+  await messaging.subscribeToTopic(topic);
+}
+
+void printReceivedFirebaseMessages() {
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    if (kDebugMode) {
+      print('Handling a foreground message: ${message.messageId}');
+      print('Message data: ${message.data}');
+      print('Message notification: ${message.notification?.title}');
+      print('Message notification: ${message.notification?.body}');
+    }
+
+    _messageStreamController.sink.add(message);
+  });
+}
+
+// It requests a registration token for sending messages to users from your App server or other trusted server environment.
+Future<void> registrationFirebaseMessaging() async {
+  // It requests a registration token for sending messages to users from your App server or other trusted server environment.
+  String? token = await messaging.getToken();
+
+  if (kDebugMode) {
+    print('Registration Token=$token');
+  }
+}
+
+/// Configured on startup for simplicity
+Future<void> requestingPermissionsFirebaseMessaging() async {
+  final settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  if (kDebugMode) {
+    print('Permission granted: ${settings.authorizationStatus}');
+  }
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+
+  if (kDebugMode) {
+    print("Handling a background message: ${message.messageId}");
+    print('Message data: ${message.data}');
+    print('Message notification: ${message.notification?.title}');
+    print('Message notification: ${message.notification?.body}');
+  }
 }
