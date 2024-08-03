ad1a02d6bcd8bfc1e03b16cf6791f70ba6ba841c
Filipe Barroso <filipe.barroso@gmail.com>
2024-08-03 20:17:03 +0100

    fix: navigation between map and detail

--- apps/fogospt/lib/routing/route.dart
+++ apps/fogospt/lib/routing/route.dart
@@ -1,9 +1,23 @@
+import 'package:fogos_api/features/latest_warnings/domain/fire.dart';
 import 'package:fogospt/features/map/presentation/map_page.dart';
+import 'package:fogospt/features/warning/presentation/warning_detail_page.dart';
 import 'package:go_router/go_router.dart';
 
 final router = GoRouter(routes: [
-  GoRoute(
-    path: '/',
-    builder: (context, state) => MapPage(),
-  ),
+  GoRoute(path: '/', builder: (context, state) => MapPage(), routes: [
+    GoRoute(
+      path: 'warning-detail',
+      builder: (context, state) {
+        final warning = state.extra;
+        if (warning == null) {
+          return const WarningDetailPage();
+        }
+        if (warning is! Fire) {
+          return const WarningDetailPage();
+        }
+
+        return WarningDetailPage(warning: warning);
+      },
+    )
+  ]),
 ]);
